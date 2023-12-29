e773b891b490c734fa9e112619403a7782206ad7
spest3122 <spest3122@gmail.com>
2023-12-29 22:35:01 +0800

    feature 1. 底部組件加入授權相關的描述與開發者 2. 移除api邏輯非必要參數

--- src/component/Logic/mdares.js
+++ src/component/Logic/mdares.js
@@ -2,7 +2,7 @@ import axios from "@/axios";
 export const mdaresApi = async () => {
   let apiResult = await axios.get("DataFileService.aspx?UnitId=106&$top=10");
   let returnArray = [];
-  apiResult.data.map((item, index) => {
+  apiResult.data.map((item) => {
     returnArray.push({
       date: item["公告日期"],
       url: item["網址"],
