29fdde835a925b31a567c2f0d6496112b9bc58d5
spest3122 <spest3122@gmail.com>
2024-01-29 15:12:59 +0800

    feature 1. 設置vite base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/spest3122.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
